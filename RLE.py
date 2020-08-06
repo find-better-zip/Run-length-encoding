7d47db1592f9136ab00484577c8813ba5e8c3fd7
blackkong8 <32637360+blackkong8@users.noreply.github.com>
2020-08-06 22:50:18 +0900

    Update RLE.py
    error

--- RLE.py
+++ RLE.py
@@ -1,10 +1,12 @@
-def RLE(info):
+def RLE_encoding(info):
     key = info.freqList.index(min(info.freqList))
     idx = 0
     new_byteArr = []
     while idx < info.fileSize:
         last = info.byteArr[idx]
         new_byteArr.append(last)
+        if last == key:
+            new_byteArr.append(last)
         idx += 1
         count = 1
         try:
@@ -19,3 +21,20 @@ def RLE(info):
         elif count == 2:
             new_byteArr.append(last)
     return key, new_byteArr
+
+def RLE_decoding(key, byteArr):
+    new_byteArr = []
+    idx = 0
+    while idx < len(byteArr):
+        last = byteArr[idx]
+        try:
+            if key == byteArr[idx+1]:
+                if key == byteArr[idx+2]:
+                    new_byteArr.append
+                else:
+                    pass
+            else:
+                new_byteArr.append(last)
+        except:
+            pass
+        
